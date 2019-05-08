dc6c0a5359e6a06fbf91363f52cb2c2b65e17dd1
Clark Williams <theclarkwilliams@gmail.com>
2019-05-08 13:48:07 -0600

    finished this.js, thought still a little fuzzy

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,60 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. window binding is when a function with "this" is called without (context?), it defaults to the window object 
+* 2. implicit binding is when you call a function, whatever is left of the function is what "".this" is referring to.
+* 3. new binding is when the "new" keyword makes "this" refer to the new object being created...   ???
+* 4. explicit binding is when you use".call()", ".apply()", and ".bind()" to assign what you want "this" to refer to.
 *
 * write out a code example of each explanation above
 */
 
-// Principle 1
+//                                      Principle 1
+//                               code example for Window Binding (done)
 
-// code example for Window Binding
+// const sayAge = function() {
+//     console.log(this.age);
+// }
+// const me = {
+//     age: 23
+// }
+// sayAge(); // logs "undefined"
 
-// Principle 2
+//                                      Principle 2
+//                              code example for Implicit Binding (done)
 
-// code example for Implicit Binding
+// const me = {
+//     name: 'Clark',
+//     age: 23,
+//     sayName: function(){
+//         console.log(this.name);
+//     }
+// }
+// me.sayName();  // logs "Clark"
 
-// Principle 3
+//                                      Principle 3
+//                              code example for New Binding
 
-// code example for New Binding
+const ConstructorFn = function(x, y , z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+}
+const thing = new ConstructorFn(1, 2, 3);
 
-// Principle 4
+//                                      Principle 4
+//                               code example for Explicit Binding (done)
 
-// code example for Explicit Binding
+// const introduce = function(lang1, lang2, lang3){
+//     console.log(`Hello, i'm ${this.name} and I speak ${lang1}, ${lang2}, and ${lang3}.`);
+// }
+
+// const bob = {
+//     name: 'Bobby',
+//     age: 30
+// }
+//  let languages = ['english', 'spanish', 'german']
+// // introduce.call(bob, languages[0], languages[1], languages[2]);
+// introduce.apply(bob, languages);
+// // let newFn = introduce.bind(bob, languages[0], languages[1], languages[2]);
+// // newFn();
