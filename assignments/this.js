15fc988b2f8ccbbf97188e9b6ec5c15b86b389d8
Clark Williams <theclarkwilliams@gmail.com>
2019-05-08 17:20:24 -0600

    mvp, many thanks to adam and brandon

--- assignments/this.js
+++ assignments/this.js
@@ -34,28 +34,29 @@
 
 //                                      Principle 3
 //                              code example for New Binding
+//         version 1 
+// const Person = function(name, age , favColor) {
+//     this.name = name;
+//     this.age = age;
+//     this.favColor = favColor;
+// }
+// const joe = new Person("Joe", 35, "blue");
+// console.log(joe);
 
-const Person = function(name, age , favColor) {
-    this.name = name;
-    this.age = age;
-    this.favColor = favColor;
-}
-const joe = new Person("Joe", 35, "blue");
-console.log(joe);
 
 //                                      Principle 4
 //                               code example for Explicit Binding (done)
 
-// const introduce = function(lang1, lang2, lang3){
-//     console.log(`Hello, i'm ${this.name} and I speak ${lang1}, ${lang2}, and ${lang3}.`);
-// }
+const introduce = function(lang1, lang2, lang3){
+    console.log(`Hello, i'm ${this.name} and I speak ${lang1}, ${lang2}, and ${lang3}.`);
+}
 
-// const bob = {
-//     name: 'Bobby',
-//     age: 30
-// }
-//  let languages = ['english', 'spanish', 'german']
-// // introduce.call(bob, languages[0], languages[1], languages[2]);
-// introduce.apply(bob, languages);
-// // let newFn = introduce.bind(bob, languages[0], languages[1], languages[2]);
-// // newFn();
+const bob = {
+    name: 'Bobby',
+    age: 30
+}
+ let languages = ['english', 'spanish', 'german']
+// introduce.call(bob, languages[0], languages[1], languages[2]);
+introduce.apply(bob, languages);
+// let newFn = introduce.bind(bob, languages[0], languages[1], languages[2]);
+// newFn();
